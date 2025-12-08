30db0faedb7864f5bf1d923321c612edef1f8e74
Hosairis <rafiei.default@gmail.com>
2025-12-08 14:36:50 +0330

    add player id cache

--- spigot/src/main/kotlin/me/hosairis/matchvault/services/TrackerService.kt
+++ spigot/src/main/kotlin/me/hosairis/matchvault/services/TrackerService.kt
@@ -1,9 +1,11 @@
 package me.hosairis.matchvault.services
 
 import de.marcely.bedwars.api.arena.Arena
+import java.util.UUID
 import java.util.concurrent.ConcurrentHashMap
 
 object TrackerService {
+    val playerIds: MutableMap<UUID, Long> = ConcurrentHashMap()
     val matchIds: MutableMap<Arena, Long> = ConcurrentHashMap()
 }
 
