cbf86798a19a9671422433d733827e3ea2262fdb
Hosairis <rafiei.default@gmail.com>
2025-12-15 16:21:31 +0330

    refactor/rework main and addon class, register database services and config

--- spigot/src/main/kotlin/me/hosairis/matchvault/MatchVault.kt
+++ spigot/src/main/kotlin/me/hosairis/matchvault/MatchVault.kt
@@ -1,40 +1,119 @@
 package me.hosairis.matchvault
 
 import me.hosairis.matchvault.storage.config.Config
+import me.hosairis.matchvault.util.CoroutineHelper
+import me.hosairis.matchvault.util.Log
 import me.hosairis.matchvault.storage.database.Db
+import me.hosairis.matchvault.storage.database.repo.MatchPlayerRepository
+import me.hosairis.matchvault.storage.database.repo.MatchRepository
+import me.hosairis.matchvault.storage.database.repo.MatchTeamRepository
+import me.hosairis.matchvault.storage.database.repo.PlayerRepository
+import me.hosairis.matchvault.storage.database.repo.ShopPurchaseRepository
+import me.hosairis.matchvault.storage.database.repo.TimelineMetaRepository
+import me.hosairis.matchvault.storage.database.repo.TimelineRepository
+import me.hosairis.matchvault.storage.database.repo.UpgradePurchaseRepository
+import me.hosairis.matchvault.storage.database.service.MatchService
+import me.hosairis.matchvault.storage.database.service.PlayerService
+import me.hosairis.matchvault.storage.database.service.PurchaseService
+import me.hosairis.matchvault.storage.database.service.TimelineService
+import me.hosairis.matchvault.tracking.listeners.match.EliminationsListener
+import me.hosairis.matchvault.tracking.listeners.match.MatchRoundListener
+import me.hosairis.matchvault.tracking.listeners.player.PlayerSessionListener
+import me.hosairis.matchvault.tracking.listeners.player.PlayerStatsListener
+import me.hosairis.matchvault.tracking.listeners.purchase.PurchaseListener
 import org.bstats.bukkit.Metrics
 import revxrsal.zapper.ZapperJavaPlugin
 
 class MatchVault: ZapperJavaPlugin() {
+
     companion object {
-        private lateinit var instance: MatchVault
-        private lateinit var addon: MatchVaultAddon
-        private lateinit var metrics: Metrics
-
-        fun getInst(): MatchVault {
-            return instance
-        }
-
-        fun getAddon(): MatchVaultAddon {
-            return addon
-        }
+        lateinit var instance: MatchVault
+        lateinit var addon: MatchVaultAddon
     }
+
+    lateinit var playerService: PlayerService
+        private set
+    lateinit var matchService: MatchService
+        private set
+    lateinit var purchaseService: PurchaseService
+        private set
+    lateinit var timelineService: TimelineService
+        private set
+
+    private var metrics: Metrics? = null
 
     override fun onEnable() {
         instance = this
-        metrics = Metrics(getInst(), 27239)
 
         addon = MatchVaultAddon(this).registerAddon()
 
-        Config.init()
+        Config.init(addon.dataFolder)
         Db.init()
 
-        addon.registerModules()
+        // 3) wire services
+        wireServices()
+
+        // 4) register listeners/commands
+        registerEntrypoints()
+
+        matchService.abortOngoingMatchesOnStartup(Config.values.serverName)
+
+        metrics = Metrics(this, 27239)
+
+        Log.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        Log.info("${this.description.name} v${this.description.version} Loaded")
+        Log.info("Developed by Hosairis (Dreamers Studios)")
+        Log.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
     }
 
     override fun onDisable() {
-        metrics.shutdown()
+        metrics?.shutdown()
+        metrics = null
+
+        CoroutineHelper.cancelAll()
         Db.close()
-        addon.unregisterModules()
+    }
+
+    private fun wireServices() {
+        // repos
+        val playerRepo = PlayerRepository()
+        val matchRepo = MatchRepository()
+        val teamRepo = MatchTeamRepository()
+        val matchPlayerRepo = MatchPlayerRepository()
+        val shopRepo = ShopPurchaseRepository()
+        val upgradeRepo = UpgradePurchaseRepository()
+        val timelineRepo = TimelineRepository()
+        val timelineMetaRepo = TimelineMetaRepository()
+
+        // services
+        playerService = PlayerService(playerRepo)
+
+        matchService = MatchService(
+            matchRepo = matchRepo,
+            teamRepo = teamRepo,
+            matchPlayerRepo = matchPlayerRepo,
+            playerRepo = playerRepo
+        )
+
+        purchaseService = PurchaseService(
+            shopRepo = shopRepo,
+            upgradeRepo = upgradeRepo
+        )
+
+        timelineService = TimelineService(
+            timelineRepo = timelineRepo,
+            metaRepo = timelineMetaRepo
+        )
+    }
+
+    private fun registerEntrypoints() {
+        val pm = server.pluginManager
+
+        // tracking listeners (inject services)
+        pm.registerEvents(PlayerSessionListener(playerService), this)
+        pm.registerEvents(PlayerStatsListener(matchService, playerService), this)
+        pm.registerEvents(MatchRoundListener(matchService), this)
+        pm.registerEvents(EliminationsListener(matchService), this)
+        pm.registerEvents(PurchaseListener(purchaseService, playerService, matchService), this)
     }
 }
--- spigot/src/main/kotlin/me/hosairis/matchvault/MatchVaultAddon.kt
+++ spigot/src/main/kotlin/me/hosairis/matchvault/MatchVaultAddon.kt
@@ -4,18 +4,13 @@ import de.marcely.bedwars.api.BedwarsAddon
 import org.bukkit.Bukkit
 
 class MatchVaultAddon(plugin: MatchVault) : BedwarsAddon(plugin) {
-    val MIN_BW_API_VER = 203
-    val MIN_BW_VER = "5.5.3"
-
-    fun registerModules() {
-        Bukkit.getLogger().info { "MB-MatchVault ready and loaded" }
-    }
-
-    fun unregisterModules() {
+    companion object {
+        const val MIN_BW_API_VER = 203
+        const val MIN_BW_VER = "5.5.3"
     }
 
     override fun getName(): String {
-        return MatchVault.getInst().description.name
+        return plugin.description.name
     }
 
     fun registerAddon(): MatchVaultAddon {
@@ -28,13 +23,13 @@ class MatchVaultAddon(plugin: MatchVault) : BedwarsAddon(plugin) {
             }
         } catch (_: Exception) {
             Bukkit.getLogger().severe("Unsupported MBedwars version detected, Please update to v$MIN_BW_VER")
-            Bukkit.getPluginManager().disablePlugin(MatchVault.getInst())
+            Bukkit.getPluginManager().disablePlugin(plugin)
         }
 
         val registered = this.register()
         if (!registered) {
             Bukkit.getLogger().severe("An error occurred, Please check for duplicate addons and remove them")
-            Bukkit.getPluginManager().disablePlugin(MatchVault.getInst())
+            Bukkit.getPluginManager().disablePlugin(plugin)
         }
 
         return this
