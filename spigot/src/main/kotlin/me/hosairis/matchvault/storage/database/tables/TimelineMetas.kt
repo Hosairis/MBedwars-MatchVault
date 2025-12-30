49ad9b94953c0bd5afbcd30eee9564f1c65f8bdf
Hosairis <rafiei.default@gmail.com>
2025-12-30 19:16:53 +0330

    removed single‑column indexes on key/value and replaced with (timeline_id, key)     to reduce index bloat.

--- spigot/src/main/kotlin/me/hosairis/matchvault/storage/database/tables/TimelineMetas.kt
+++ spigot/src/main/kotlin/me/hosairis/matchvault/storage/database/tables/TimelineMetas.kt
@@ -4,7 +4,8 @@ import org.jetbrains.exposed.v1.core.ReferenceOption
 import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
 
 object TimelineMetas : LongIdTable("timeline_metas") {
-    val timelineId = reference("timeline_id", Timelines, onDelete = ReferenceOption.CASCADE).index()
-    val key = varchar("key", 64).index()
-    val value = varchar("value", 255).index()
+    val timelineId = reference("timeline_id", Timelines, onDelete = ReferenceOption.CASCADE)
+    val key = varchar("key", 64)
+    val value = varchar("value", 255)
+    init { index(false, timelineId, key) }
 }
