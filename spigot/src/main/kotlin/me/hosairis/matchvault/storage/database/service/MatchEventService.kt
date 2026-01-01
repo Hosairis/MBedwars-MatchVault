43677eba61dfd5cddb9062d7e10ed870a2b95562
Hosairis <rafiei.default@gmail.com>
2026-01-01 20:34:17 +0330

    refactor database repository code

---
+++ spigot/src/main/kotlin/me/hosairis/matchvault/storage/database/service/MatchEventService.kt
@@ -0,0 +1,41 @@
+package me.hosairis.matchvault.storage.database.service
+
+import me.hosairis.matchvault.storage.database.model.MatchEventData
+import me.hosairis.matchvault.storage.database.model.MatchEventMetaData
+import me.hosairis.matchvault.storage.database.repo.MatchEventMetaRepository
+import me.hosairis.matchvault.storage.database.repo.MatchEventRepository
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+class MatchEventService(
+    private val matchEventRepo: MatchEventRepository,
+    private val metaRepo: MatchEventMetaRepository
+) {
+
+    /**
+     * Create a timeline event and optional metas in ONE transaction.
+     * Returns created timelineId.
+     */
+    fun createEvent(event: MatchEventData, metas: List<MatchEventMetaData> = emptyList()): Long = transaction {
+        val timelineId = matchEventRepo.create(event.copy(id = null))
+        metas.forEach { m ->
+            metaRepo.create(m.copy(id = null, matchEventId = timelineId))
+        }
+        timelineId
+    }
+
+    fun readEvent(id: Long): MatchEventData? = transaction {
+        matchEventRepo.read(id)
+    }
+
+    fun readEventsByMatchId(matchId: Long): List<MatchEventData> = transaction {
+        matchEventRepo.readByMatchId(matchId)
+    }
+
+    fun readMetasByTimelineId(timelineId: Long): List<MatchEventMetaData> = transaction {
+        metaRepo.readByMatchEventId(timelineId)
+    }
+
+    fun deleteEvent(id: Long): Boolean = transaction {
+        matchEventRepo.delete(id)
+    }
+}
