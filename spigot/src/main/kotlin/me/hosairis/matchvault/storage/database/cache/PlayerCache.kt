c59af633e10844cc634321658dcbca63f45126bb
Hosairis <amir.h.rafiei2001@gmail.com>
2026-01-26 15:12:02 +0330

    tweak player cache

--- spigot/src/main/kotlin/me/hosairis/matchvault/storage/database/cache/PlayerCache.kt
+++ spigot/src/main/kotlin/me/hosairis/matchvault/storage/database/cache/PlayerCache.kt
@@ -5,14 +5,19 @@ import java.util.concurrent.ConcurrentHashMap
 
 internal object PlayerCache {
     private val idByUuid = ConcurrentHashMap<UUID, Long>()
+    private val idByName = ConcurrentHashMap<String, Long>()
 
     fun getId(uuid: UUID): Long? = idByUuid[uuid]
 
-    fun put(uuid: UUID, id: Long) {
+    fun getId(name: String): Long? = idByName[name]
+
+    fun put(uuid: UUID, name: String, id: Long) {
         idByUuid[uuid] = id
+        idByName[name] = id
     }
 
-    fun remove(uuid: UUID) {
+    fun remove(uuid: UUID, name: String) {
         idByUuid.remove(uuid)
+        idByName.remove(name)
     }
 }
